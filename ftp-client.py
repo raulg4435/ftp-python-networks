a3e9c9f8dc8122e62df296bcebc8809af437b48e
Raul Garcia <raul.g4435@gmail.com>
2020-11-28 20:25:14 -0600

    Add files via upload

---
+++ ftp-client.py
@@ -0,0 +1,27 @@
+# import FP class, which implements client side of FTP
+from ftplib import FTP
+
+# connect to server and login
+ftp = FTP('')
+ftp.connect('localhost', 1026)              # connect to host, port
+#ftp.login()                                # login (anonymous)
+ftp.login(user='hacker', passwd='12345')    # login with full access
+ftp.cwd('books')                            # change working directory
+ftp.retrlines('LIST')                       # FTP command to list directory contents
+
+def uploadFile():
+    filename = 'hunger_games.txt'
+    # store file in binary transfer mode, opening in 'rb' mode: "read-binary"
+    ftp.storbinary('STOR '+ filename, open(filename, 'rb')) 
+    ftp.quit()
+
+def downloadFile():
+    filename = 'odyssey.txt'
+    localfile = open(filename, 'wb')                            # open in 'wb' mode: "write-binary"
+    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)   # retrieve file, use port
+    ftp.quit()
+    localfile.close()
+
+# Un-comment the function you'd like to execute
+uploadFile()
+#downloadFile()
---
+++ ftp-server.py
@@ -0,0 +1,29 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+# Instantiate a dummy authorizer for managing 'virtual' users
+authorizer = DummyAuthorizer()
+
+# Define a new user having full r/w permissions and a read-only
+# anonymous user
+authorizer.add_user('hacker', '12345', '.', perm='elradfmwMT')
+authorizer.add_anonymous(os.getcwd())
+
+ # Instantiate FTP handler clas
+handler = FTPHandler
+handler.authorizer = authorizer
+
+# Define a customized banner (string returned when client connects)
+handler.banner = "FTP server ready"
+
+# Instantiate FTP server class and listen on the given IP address and port
+address = ("127.0.0.1", 1026)
+server = FTPServer(address, handler)
+
+# set a limit for connections and start the FTP server
+server.max_cons = 256
+server.max_cons_per_ip = 5
+server.serve_forever()
